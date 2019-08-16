ce174c026599e1b9a4f7e8c0063c1c1f9fd0b304
drubicza <id.slacker@gmail.com>
2019-08-16 16:27:24 +0800

    Add deobfuscated source

---
+++ sadapakas-deobfuscated.sh
@@ -0,0 +1,153 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+st_st=$(date +%A)
+ss_ss=$(date +%H)
+se_se=$(date +%M)
+sw_sw=$(date +%S)
+es_es=$(date +%d)
+rt_rt=$(date +%B)
+echo $blue"╔════════════════════════════════════════════════════════════════════════════╗"
+echo $blue"║$red  BULAN$green :$yellow $rt_rt$red TANGGAL$green :$yellow $es_es$red HARI$green :$yellow $st_st$red JAM$green :$yellow $ss_ss$red MENIT$green :$yellow $se_se$red DETIK$green :$yellow $sw_sw$blue  ║"
+echo $blue"╚════════════════════════════════════════════════════════════════════════════╝"
+echo $cyan"╔═════════════════╗   ╔═════════════════════════════════════════════════╗"
+echo $cyan"║$white█████████████████$cyan║   ║$green [*] DESIGN : PRAKASA_JR64 AUTHOR : PRAKASA_JR64$cyan ║"
+echo $cyan"║$white████$cyan╔═══╗$white██$cyan╔═══╗$white█$cyan║   ║$green [*] WHATSAPP : +62-857-7951-5200 TWITER : NIHIL$cyan ║"
+echo $cyan"║$white████$cyan║$red ⓿$cyan ║$white██$cyan║$red ⓿$cyan ║$white█$cyan║   ║$green [*] FACEBOOK : PRAKASA_JR64 || SCRIPT BOSS AKAS$cyan ║"
+echo $cyan"║$white████$cyan╚═══╝$white██$cyan╚═══╝$white█$cyan║   ╠═════════════════════════════════════════════════╣"
+echo $cyan"║$white█████████$cyan║║$white██████$cyan║   ║$green [+] JIKA TIDAK TAHU CODE KEAMANANNYA TANYAKAN$cyan   ║"
+echo $cyan"╚═╗$white██████████████$cyan═╝   ║$green KEPADA NOMER WHATSAPP : +62857-7951-5200 THANKS$cyan ║"
+echo $cyan"  ╚═╬╩╩╩╩╩╩╩╩╩╩╩╝     ╚═════════════════════════════════════════════════╝"
+echo $cyan"    ║$white───█ ██████$red▒$blue oo0"
+echo $cyan"    ╚═╦╦╦╦╦╦╦╦╗"
+echo $cyan"      ╚$white████████$cyan:"
+echo $red"═════════════════════════════════════════════════════════════════════════"
+echo $yellow"KUNJUNGI SEMUA SOSIAL MEDIA SAYA YA SAYANG DAN JANGAN LUPA ADD,SVE,FOLLOW"
+echo $green
+read -p "[ROOT@PRAKASA_JR64]CODE KEAMANANNYA MASUKAN SAYANG : " saya;
+if [ $saya = PRAKASA_JR64 ]; then
+clear
+echo $blue"╔════════════════════════════════════════════════════════════════════════════╗"
+echo $blue"║$red  BULAN$green :$yellow $rt_rt$red TANGGAL$green :$yellow $es_es$red HARI$green :$yellow $st_st$red JAM$green :$yellow $ss_ss$red MENIT$green :$yellow $se_se$red DETIK$green :$yellow $sw_sw$blue  ║"
+echo $blue"╚════════════════════════════════════════════════════════════════════════════╝"
+echo $cyan"╔═════════════════╗   ╔═════════════════════════════════════════════════╗"
+echo $cyan"║$white█████████████████$cyan║   ║$green [*] DESIGN : PRAKASA_JR64 AUTHOR : PRAKASA_JR64$cyan ║"
+echo $cyan"║$white████$cyan╔═══╗$white██$cyan╔═══╗$white█$cyan║   ║$green [*] WHATSAPP : +62-857-7951-5200 TWITER : NIHIL$cyan ║"
+echo $cyan"║$white████$cyan║$red ⓿$cyan ║$white██$cyan║$red ⓿$cyan ║$white█$cyan║   ║$green [*] FACEBOOK : PRAKASA_JR64 || SCRIPT BOSS AKAS$cyan ║"
+echo $cyan"║$white████$cyan╚═══╝$white██$cyan╚═══╝$white█$cyan║   ╠═════════════════════════════════════════════════╣"
+echo $cyan"║$white█████████$cyan║║$white██████$cyan║   ║$green [+] JIKA TIDAK TAHU CODE KEAMANANNYA TANYAKAN$cyan   ║"
+echo $cyan"╚═╗$white██████████████$cyan═╝   ║$green KEPADA NOMER WHATSAPP : +62857-7951-5200 THANKS$cyan ║"
+echo $cyan"  ╚═╬╩╩╩╩╩╩╩╩╩╩╩╝     ╚═════════════════════════════════════════════════╝"
+echo $cyan"    ║$white───█ ██████$red▒$blue oo0"
+echo $cyan"    ╚═╦╦╦╦╦╦╦╦╗"
+echo $cyan"      ╚$white████████$cyan:"
+echo $red"═════════════════════════════════════════════════════════════════════════"
+echo $yellow"  PRAKASA_JR64 TELAH MENYIAPKAN (3)SCRIPT SADAP FACEBOOK BUAT KALIAN :*"
+echo $cyan
+echo $cyan"╔═══════════════════════════════════════════════════════════════════╗"
+echo $cyan"║ [1]>INSTALL BAHAN [2]>SADAP FB(1) [3]>SADAP FB(2) [4]>SADAP FB(3) ║"
+echo $cyan"╚═══════════════════════════════════════════════════════════════════╝"
+echo $green
+read -p "[ROOT@PRAKASA_JR64]PILIH NOMERNYA BOSS : " sadapkas;
+fi
+if [ $sadapkas = 1 ]; then
+pkg install git
+apt install git
+pip2 install mechanize
+pip2 install module
+pip2 install -r requirements.txt
+pkg install python
+pkg install python2
+pkg install php
+pkg install bash
+echo $green
+read -p "[ROOT@PRAKASA_JR64]KEMBALI KE HALAMAN UTAMA KETIK Y/N : " yes;
+fi
+if [ $sadapkas = 2 ]; then
+clear
+echo $blue"╔════════════════════════════════════════════════════════════════════════════╗"
+echo $blue"║$red  BULAN$green :$yellow $rt_rt$red TANGGAL$green :$yellow $es_es$red HARI$green :$yellow $st_st$red JAM$green :$yellow $ss_ss$red MENIT$green :$yellow $se_se$red DETIK$green :$yellow $sw_sw$blue  ║"
+echo $blue"╚════════════════════════════════════════════════════════════════════════════╝"
+echo $cyan"╔═════════════════╗   ╔═════════════════════════════════════════════════╗"
+echo $cyan"║$white█████████████████$cyan║   ║$green [*] DESIGN : PRAKASA_JR64 AUTHOR : PRAKASA_JR64$cyan ║"
+echo $cyan"║$white████$cyan╔═══╗$white██$cyan╔═══╗$white█$cyan║   ║$green [*] WHATSAPP : +62-857-7951-5200 TWITER : NIHIL$cyan ║"
+echo $cyan"║$white████$cyan║$red ⓿$cyan ║$white██$cyan║$red ⓿$cyan ║$white█$cyan║   ║$green [*] FACEBOOK : PRAKASA_JR64 || SCRIPT BOSS AKAS$cyan ║"
+echo $cyan"║$white████$cyan╚═══╝$white██$cyan╚═══╝$white█$cyan║   ╠═════════════════════════════════════════════════╣"
+echo $cyan"║$white█████████$cyan║║$white██████$cyan║   ║$green [+] JIKA TIDAK TAHU CODE KEAMANANNYA TANYAKAN$cyan   ║"
+echo $cyan"╚═╗$white██████████████$cyan═╝   ║$green KEPADA NOMER WHATSAPP : +62857-7951-5200 THANKS$cyan ║"
+echo $cyan"  ╚═╬╩╩╩╩╩╩╩╩╩╩╩╝     ╚═════════════════════════════════════════════════╝"
+echo $cyan"    ║$white───█ ██████$red▒$blue oo0"
+echo $cyan"    ╚═╦╦╦╦╦╦╦╦╗"
+echo $cyan"      ╚$white████████$cyan:"
+echo $red"═════════════════════════════════════════════════════════════════════════"
+echo $yellow" SCRIPT SADAP FACEBOOK YANG PERTAMA KUNJUNGI CHANEL MRTAMFANX CYBER TEAM"
+echo
+sleep 0.8
+echo $yellow"[+]MOHON TUNGGU SEBENTAR..."
+sleep 5
+git clone https://github.com/Pashayogi/SETAN
+cd SETAN
+pip2 install mechanize
+python2 SETAN.py
+fi
+if [ $sadapkas = 3 ]; then
+clear
+echo $blue"╔════════════════════════════════════════════════════════════════════════════╗"
+echo $blue"║$red  BULAN$green :$yellow $rt_rt$red TANGGAL$green :$yellow $es_es$red HARI$green :$yellow $st_st$red JAM$green :$yellow $ss_ss$red MENIT$green :$yellow $se_se$red DETIK$green :$yellow $sw_sw$blue  ║"
+echo $blue"╚════════════════════════════════════════════════════════════════════════════╝"
+echo $cyan"╔═════════════════╗   ╔═════════════════════════════════════════════════╗"
+echo $cyan"║$white█████████████████$cyan║   ║$green [*] DESIGN : PRAKASA_JR64 AUTHOR : PRAKASA_JR64$cyan ║"
+echo $cyan"║$white████$cyan╔═══╗$white██$cyan╔═══╗$white█$cyan║   ║$green [*] WHATSAPP : +62-857-7951-5200 TWITER : NIHIL$cyan ║"
+echo $cyan"║$white████$cyan║$red ⓿$cyan ║$white██$cyan║$red ⓿$cyan ║$white█$cyan║   ║$green [*] FACEBOOK : PRAKASA_JR64 || SCRIPT BOSS AKAS$cyan ║"
+echo $cyan"║$white████$cyan╚═══╝$white██$cyan╚═══╝$white█$cyan║   ╠═════════════════════════════════════════════════╣"
+echo $cyan"║$white█████████$cyan║║$white██████$cyan║   ║$green [+] JIKA TIDAK TAHU CODE KEAMANANNYA TANYAKAN$cyan   ║"
+echo $cyan"╚═╗$white██████████████$cyan═╝   ║$green KEPADA NOMER WHATSAPP : +62857-7951-5200 THANKS$cyan ║"
+echo $cyan"  ╚═╬╩╩╩╩╩╩╩╩╩╩╩╝     ╚═════════════════════════════════════════════════╝"
+echo $cyan"    ║$white───█ ██████$red▒$blue oo0"
+echo $cyan"    ╚═╦╦╦╦╦╦╦╦╗"
+echo $cyan"      ╚$white████████$cyan:"
+echo $red"═════════════════════════════════════════════════════════════════════════"
+echo $yellow"  SCRIPT SADAP FACEBOOK YANG KEDUA KUNJUNGI CHANEL MRTAMFANX CYBER TEAM"
+echo
+sleep 0.8
+echo $yellow"[+]MOHON TUNGGU SEBENTAR..."
+sleep 5
+git clone https://github.com/pirmansx/mbf
+cd mbf
+python2 mbf.py
+fi
+if [ $sadapkas = 4 ]; then
+clear
+echo $blue"╔════════════════════════════════════════════════════════════════════════════╗"
+echo $blue"║$red  BULAN$green :$yellow $rt_rt$red TANGGAL$green :$yellow $es_es$red HARI$green :$yellow $st_st$red JAM$green :$yellow $ss_ss$red MENIT$green :$yellow $se_se$red DETIK$green :$yellow $sw_sw$blue  ║"
+echo $blue"╚════════════════════════════════════════════════════════════════════════════╝"
+echo $cyan"╔═════════════════╗   ╔═════════════════════════════════════════════════╗"
+echo $cyan"║$white█████████████████$cyan║   ║$green [*] DESIGN : PRAKASA_JR64 AUTHOR : PRAKASA_JR64$cyan ║"
+echo $cyan"║$white████$cyan╔═══╗$white██$cyan╔═══╗$white█$cyan║   ║$green [*] WHATSAPP : +62-857-7951-5200 TWITER : NIHIL$cyan ║"
+echo $cyan"║$white████$cyan║$red ⓿$cyan ║$white██$cyan║$red ⓿$cyan ║$white█$cyan║   ║$green [*] FACEBOOK : PRAKASA_JR64 || SCRIPT BOSS AKAS$cyan ║"
+echo $cyan"║$white████$cyan╚═══╝$white██$cyan╚═══╝$white█$cyan║   ╠═════════════════════════════════════════════════╣"
+echo $cyan"║$white█████████$cyan║║$white██████$cyan║   ║$green [+] JIKA TIDAK TAHU CODE KEAMANANNYA TANYAKAN$cyan   ║"
+echo $cyan"╚═╗$white██████████████$cyan═╝   ║$green KEPADA NOMER WHATSAPP : +62857-7951-5200 THANKS$cyan ║"
+echo $cyan"  ╚═╬╩╩╩╩╩╩╩╩╩╩╩╝     ╚═════════════════════════════════════════════════╝"
+echo $cyan"    ║$white───█ ██████$red▒$blue oo0"
+echo $cyan"    ╚═╦╦╦╦╦╦╦╦╗"
+echo $cyan"      ╚$white████████$cyan:"
+echo $red"═════════════════════════════════════════════════════════════════════════"
+echo $yellow" SCRIPT SADAP FACEBOOK YANG KETIGA KUNJUNGI CHANEL MRTAMFANX CYBER TEAM"
+echo
+sleep 0.8
+echo $yellow"[+]MOHON TUNGGU SEBENTAR..."
+sleep 5
+git clone https://github.com/MrTamfanX/TamfanFacebook
+cd TamfanFacebook
+chmod +x *
+chmod +x -R 775 lib
+sh instal.sh
+sh tamfan.sh
+fi
